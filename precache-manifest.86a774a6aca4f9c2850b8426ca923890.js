fcf53544f58b28f8a5e186cb38c0930c17d42f50
SilvenLEAF <SilvenLEAF@gmail.com>
2020-09-29 18:31:27 +0530

    Updates

---
+++ precache-manifest.86a774a6aca4f9c2850b8426ca923890.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4249278cf903f52a02b6118e4450e1b",
+    "url": "/AsteriodsNearEarthAPP/index.html"
+  },
+  {
+    "revision": "b4fbb5f7db05d8773f2f",
+    "url": "/AsteriodsNearEarthAPP/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "3edc64882c72d7c83992",
+    "url": "/AsteriodsNearEarthAPP/static/css/main.39d8fd8f.chunk.css"
+  },
+  {
+    "revision": "b4fbb5f7db05d8773f2f",
+    "url": "/AsteriodsNearEarthAPP/static/js/2.708abb35.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/AsteriodsNearEarthAPP/static/js/2.708abb35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3edc64882c72d7c83992",
+    "url": "/AsteriodsNearEarthAPP/static/js/main.70e9737c.chunk.js"
+  },
+  {
+    "revision": "1ff540220adc1ae14957",
+    "url": "/AsteriodsNearEarthAPP/static/js/runtime-main.09cf52c8.js"
+  }
+]);
