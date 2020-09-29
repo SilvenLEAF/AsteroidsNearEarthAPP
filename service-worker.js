21142d971d6d70ac550b2349e3bb7f799232d3cd
SilvenLEAF <SilvenLEAF@gmail.com>
2020-09-29 18:59:52 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AsteriodsNearEarthAPP/precache-manifest.86a774a6aca4f9c2850b8426ca923890.js"
+  "/AsteroidsNearEarthAPP/precache-manifest.054089dc6b4d765fd4bd05b4679366af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AsteriodsNearEarthAPP/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AsteroidsNearEarthAPP/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
